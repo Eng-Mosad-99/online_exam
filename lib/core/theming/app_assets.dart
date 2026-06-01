858985d60cdc64192d3e367efff4b60dc0b98ed0
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-01 13:04:20 -0400

    Add asset paths for images and icons in theming module
    - Introduced AppImages class with paths for onboarding images.
    - Introduced AppIcons class with paths for various icons used in the application.

---
+++ lib/core/theming/app_assets.dart
@@ -0,0 +1,22 @@
+
+
+class AppImages {
+  static const String onboarding1Image = 'assets/images/onboarding1.png';
+  static const String onboarding2Image = 'assets/images/onboarding2.png';
+  static const String onboarding3Image = 'assets/images/onboarding3.png';
+}
+
+class AppIcons {
+  static const String confirmPassIcon = 'assets/icons/confirm_pass_icon.svg';
+  static const String emailIcon = 'assets/icons/email_icon.svg';
+  static const String eyeIcon = 'assets/icons/eye_icon.svg';
+  static const String leftArrowIcon = 'assets/icons/left_arrow_icon.svg';
+  static const String rightArrowIcon = 'assets/icons/right_arrow_icon.svg';
+  static const String notificationIcon = 'assets/icons/notification_icon.svg';
+  static const String onlineExamIcon = 'assets/icons/online_exam_icon.svg';
+  static const String passIcon = 'assets/icons/pass_icon.svg';
+  static const String phoneIcon = 'assets/icons/phone_icon.svg';
+  static const String timerIcon = 'assets/icons/timer_icon.svg';
+  static const String usernameIcon = 'assets/icons/username_icon.svg';
+  static const String verifyOtpIcon = 'assets/icons/verify_otp_icon.svg';
+}
--- lib/features/onboarding/ui/onboarding_screen.dart
+++ lib/features/onboarding/ui/onboarding_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:online_exam/core/helper/spacing.dart';
+import 'package:online_exam/core/theming/app_assets.dart';
 
 class OnboardingScreen extends StatelessWidget {
   const OnboardingScreen({super.key});
@@ -6,7 +9,25 @@ class OnboardingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16.0.w, vertical: 20.h),
+          child: Column(
+            children: <Widget>[
+              ClipRRect(
+                borderRadius: BorderRadius.circular(32.r),
+                child: Image.asset(
+                  AppImages.onboarding1Image,
+                  width: double.infinity,
+                  height: 358.h,
+                  fit: BoxFit.fill,
+                ),
+              ),
+            
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
