da980f9302d708cdd6f63b9f5ddd897a0e5c3523
Eng-Mosad-99 <mohammedmosaad869@gmail.com>
2026-06-01 11:59:58 -0400

    change flutter native splash screen

--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:online_exam/core/di/service_locator.dart';
+import 'package:online_exam/core/routing/routes.dart';
+
+import '../../features/auth/logic/login_cubit/login_cubit.dart';
+import '../../features/auth/ui/login/login_screen.dart';
 
 class AppRouter {
   Route? onGenerateRoute(RouteSettings settings) {
@@ -8,13 +14,13 @@ class AppRouter {
     switch (settings.name) {
       // case Routes.onBoardingScreen:
       //   return MaterialPageRoute(builder: (_) => const OnboardingScreen());
-      // case Routes.loginScreen:
-      //   return MaterialPageRoute(
-      //     builder: (_) => BlocProvider(
-      //       create: (context) => getIt<LoginCubit>(),
-      //       child: const LoginScreen(),
-      //     ),
-      //   );
+      case Routes.loginScreen:
+        return MaterialPageRoute(
+          builder: (_) => BlocProvider(
+            create: (context) => getIt<LoginCubit>(),
+            child: const LoginScreen(),
+          ),
+        );
       // case Routes.signupScreen:
       //   return MaterialPageRoute(
       //     builder: (_) => BlocProvider(
